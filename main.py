ca7c65835551078bcc35f01a4780a0e9fdea8d6a
Vanessa <vanessa.ssoares.vs@gmail.com>
2023-11-26 10:26:09 -0300

    sort itens at guloso

--- main.py
+++ main.py
@@ -43,29 +43,40 @@ def get_min_value(l):
     return min_value
 
 def guloso(items, capacity):
+    sorted_items = sorted(items)
     bins = [[] for _ in range(capacity)]
 
     for b in bins:
         isNotFull = True
 
-        if len(items) == 0:
+        if len(sorted_items) == 0:
             break
 
         while isNotFull:
             bin_content = sum(b)
             free_space = capacity - bin_content
 
-            min_item = get_min_value(items)
+            if len(sorted_items) > 0:
+                min_item = sorted_items[0]
 
             if min_item <= free_space:
                 b.append(min_item)
-                items.remove(min_item)
+                sorted_items.remove(min_item)
             else:
                 isNotFull = False
 
     space_waste = sum(capacity - sum(bin_contents) for bin_contents in bins)
 
     return bins, space_waste
+
+def print_solution(solution, waste, label):
+    print('*' * 20)
+    print(label)
+    print('*' * 20)
+    for i, bin_contents in enumerate(solution):
+        print(f"Bin {i + 1}: {bin_contents}")
+
+    print(f"Total waste of this combination: {waste}")
 
 items_to_pack = [2, 5, 4, 7, 1, 1, 8, 10]
 capacity = 10
@@ -74,20 +85,10 @@ items_permutations = get_all_permutations(items_to_pack, len(items_to_pack))
 
 # Força bruta
 solution, waste = bruteforce(items_permutations, capacity)
-
-print('*'*20)
-print('Força bruta')
-for i, bin_contents in enumerate(solution):
-    print(f"Bin {i + 1}: {bin_contents}")
-
-print(f"Total waste of this combination: {waste}")
+print_solution(solution, waste, 'força bruta')
 
 # Guloso
 solution, waste = guloso(items_to_pack, capacity)
+print_solution(solution, waste, 'guloso')
 
-print('*'*20)
-print('Guloso')
-for i, bin_contents in enumerate(solution):
-    print(f"Bin {i + 1}: {bin_contents}")
 
-print(f"Total waste of this combination: {waste}")
