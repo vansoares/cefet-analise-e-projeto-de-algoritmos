bae5f14c81046ee4098e3c3f50c5e86fa3d7f3fe
Vanessa <vanessa.ssoares.vs@gmail.com>
2023-12-08 09:14:37 -0300

    adjusting time count

--- main.py
+++ main.py
@@ -27,12 +27,19 @@ def get_n_random_arrays(n, max_range, n_items):
         result.append(get_random_array(max_range, n_items))
     return result
 
+def save_metrics(brute_force_time, greedy_time, metrics_dict):
+    print(f"Força bruta: \n\t Media:{np.mean(brute_force_time)} \n\t Min:{np.min(brute_force_time)} \n\t Max:{np.max(brute_force_time)}")
+    print(f"Guloso: \n\t Media:{np.mean(greedy_time)} \n\t Min:{np.min(greedy_time)} \n\t Max:{np.max(greedy_time)}")
+    with open(f"result-{n_loops}-{n_items}.json", 'w') as arquivo:
+        json.dump(metrics_dict, arquivo)
+    print("Arquivo  salvo! ")
+
 
 capacity = 100
 brute_force_time = []
 greedy_time = []
-n_loops = 3
-n_items = 5    
+n_loops = 5
+n_items = 9    
 metrics_dict = {}
 items = get_n_random_arrays(n_loops, capacity-1, n_items)
 
@@ -47,12 +54,11 @@ for items_to_pack in items:
 
     inicio = time.time()
     solution, waste = brute_force.run_with_weight(items_permutations, capacity)
-    total_time = time.time()-inicio
-    total_time_fmt = f"{total_time:.6f}"
+    total_time_fmt = f"{time.time()-inicio:.6f}"
 
     print_solution(solution, waste, 'força bruta', total_time_fmt)
     
-    brute_force_time.append(total_time)
+    brute_force_time.append(float(total_time_fmt))
     metrics_dict[i]["brute_force"] = {"itens":items_to_pack, "total_time": total_time_fmt, "waste": waste, "solution":solution}
 
 
@@ -62,17 +68,12 @@ for items_to_pack in items:
     inicio = time.time()
 
     solution, waste = greedy.run_with_weight(items_to_pack, capacity)
-    total_time = time.time()-inicio
-    total_time_fmt = f"{total_time:.6f}"
+    total_time_fmt = f"{time.time()-inicio:.10f}"
 
     print_solution(solution, waste, 'greedy', total_time_fmt)
-    greedy_time.append(total_time)
+    greedy_time.append(float(total_time_fmt))
     metrics_dict[i]["greedy"] = {"itens":items_to_pack, "total_time": total_time_fmt, "waste": waste, "solution":solution}
 
+save_metrics(brute_force_time, greedy_time, metrics_dict)
 
 
-print(f"Força bruta: \n\t Media:{np.mean(brute_force_time)} \n\t Min:{np.min(brute_force_time)} \n\t Max:{np.max(brute_force_time)}")
-print(f"Guloso: \n\t Media:{np.mean(greedy_time)} \n\t Min:{np.min(greedy_time)} \n\t Max:{np.max(greedy_time)}")
-with open(f"result-{n_loops}-{n_items}.json", 'w') as arquivo:
-    json.dump(metrics_dict, arquivo)
-print("Arquivo  salvo! ")
--- notes.md
+++ notes.md
@@ -3,11 +3,12 @@
 - TODO
     - Implementar abordagem com peso e valor
     - Testar diferentes pesos e tamanhos de mochila
+    - resolver problema da notação cientifica do tempo do guloso
 
 ### Força bruta 
 - Máximo de 11 itens que retornam algum resultado
 - Com 12 itens na segunda iteração já da erro
-
+- Com poucos itens, chega a ser mais rapido que o guloso. Mas conforme o numero de itens aumenta, vai ficando mais lento
 
 ### Guloso
 - Roda com 600k itens (máximo que testei por enquanto)
